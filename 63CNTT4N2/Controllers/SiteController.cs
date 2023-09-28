8eac3c800280f9687274dc65c32dc91e57c788ba
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2023-09-28 23:53:17 +0700

    Buoi thu 2

--- 63CNTT4N2/Controllers/SiteController.cs
+++ 63CNTT4N2/Controllers/SiteController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyClass.Model;
 
 namespace _63CNTT4N2.Controllers
 {
     public class SiteController : Controller
     {
-        // GET: Site
         public ActionResult Index()
         {
+            MyDBContext db = new MyDBContext();
+            int sodong = db.Products.Count();
+            ViewBag.sodong = sodong;
             return View();
+
         }
     }
 }
